67147e9bfeb59ccdc83c62cd8585d3586a3e613b
420847750 <420847750@qq.com>
2026-05-12 16:06:20 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,5 +2,6 @@ import { serve } from "https://deno.land/std@0.180.0/http/server.ts";
 import { handler } from "./proxy.ts";
 
 // 关键：监听 Deno Deploy 要求的 0.0.0.0 和 PORT 环境变量
-const port = parseInt(Deno.env.get("PORT") || "8000");
-serve(handler, { port: port, hostname: "0.0.0.0" });
+//const port = parseInt(Deno.env.get("PORT") || "8888");
+serve(handler, { port: 8888 });
+//serve(handler, { port: port, hostname: "0.0.0.0" });
